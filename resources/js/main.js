a5187b52d2a3e937fe317292eebbabc46c1b36ad
Boris84 <simon.789@hotmail.co.uk>
2018-12-28 13:03:34 +0000

    update 17 pause fix final autoPlay issue

--- resources/js/main.js
+++ resources/js/main.js
@@ -6,85 +6,90 @@ showSlides(slideIndex);
 
 // next / prev controls
 function plusSlides(n) {
-  showSlides(slideIndex += n);
+showSlides(slideIndex += n);
 }
 
 // image function call 
 function currentSlide(n) {
-  showSlides(slideIndex = n);	
+showSlides(slideIndex = n);	
 }
     
 function autoSlides() {
-    var x;
-    var slides = document.getElementsByClassName("image");
+var x;
+var slides = document.getElementsByClassName("image");
 }
 
-  var slideIndex = 0;
+var slideIndex = 0;
 
 
 function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("image");
-   if (n > slides.length) {slideIndex = 1} 
-   if (n < 1) {slideIndex = slides.length}
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none"; 
-   }
+var i;
+var slides = document.getElementsByClassName("image");
+  if (n > slides.length) {slideIndex = 1} 
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none"; 
+  }
     slides[slideIndex-1].style.display = "block"; 
 
 }
 
-
-
 function playPause() { 
-  document.getElementById("play").style.display = "block";
-  var element = document.getElementById("play");
-    element.classList.toggle('pauseB');
-    document.querySelector('#element');
+document.getElementById("play").style.display = "block";
+var element = document.getElementById("play");
+element.classList.toggle('pauseB');
+document.querySelector('#element');
    
-
-    if (element === 'playB') {
-      element.classList.remove('pauseB');
-    }
-    else {
-        element.classList.add('playB');
-    }  
- 
+  if (element === 'playB') {
+    element.classList.remove('pauseB');
+  }
+  else {
+    element.classList.add('playB');
+  }  
 }
-
+  
+var playing = 
 
 document.getElementById("play").onclick = function() {
-  var slides = document.getElementsByClassName("image");
-  var slideIndex = 0;
-  var i;
-  
+var slides = document.getElementsByClassName("image");
+var slideIndex = 0;
+var i;
+
 function autoPlay(n) {
    
-   if (n < 1) {
-     slideIndex = slides.length;
-   }
-   for (i = 0; i < slides.length; i+=1) {
-        slides[i].style.display = "block"; 
-    }
-          plusSlides(1);     
-    }
-       window.setInterval(autoPlay, 3000);
-
-   
-document.getElementById("play").onclick = function() {
-  var slides = document.getElementsByClassName("image");
-    
-    if (onclick() === true) {
-          plusSlides(1).pause();
-     }
-
-   
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i+=1) {
+    slides[i].style.display = "block";
+  }
+    plusSlides(1); 
+    playing = true; 
+  
+}
+    var myTimer =  window.setInterval(autoPlay, 3000);
 
 
+
+document.getElementById("play").onclick = pauseSlideShow;
+var slides = document.getElementsByClassName("image");
+
+function pauseSlideShow(n) {
+
+  if (playing) {
+    clearInterval(myTimer);
+    playing = false;
+  }
+  if (playing == false) {
+    document.getElementById("play").onclick = autoPlay;
+    window.setInterval(autoPlay, 3000);
+  }
 }
 
+};
 
- };
+
+
 
 
 
